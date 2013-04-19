71426025bba1c9aafbd2e149fc126a82afd7b615
wesleymusgrove <wesley.p.musgrove@gmail.com>
2013-04-19 17:00:16 -0500

    Initial commit

---
+++ assets/js/collections/todos.js
@@ -0,0 +1,37 @@
+// assets/js/collections/todos.js
+
+var app = app || {};
+
+// Todo Collection
+// ---------------
+
+var TodoList = Backbone.Collection.extend({
+
+	model: app.Todo,
+
+	localStorage: new Backbone.LocalStorage('todos-backbone'),
+
+	completed: function() {
+		return this.filter(function( todo ) {
+			return todo.get('completed');
+		});
+	},
+
+	remaining: function() {
+		return this.without.apply( this, this.completed() );
+	},
+
+	nextOrder: function() {
+		if ( !this.length ) {
+			return 1;
+		}
+		return this.last().get('order') + 1;
+	},
+
+	comparator: function( todo ) {
+		return todo.get('order');
+	}
+
+});
+
+app.Todos = new TodoList();
---
+++ assets/js/models/todo.js
@@ -0,0 +1,21 @@
+//assets/js/models/todo.js
+
+var app = app || {};
+
+// Todo Model
+// ----------
+// Our basic **Todo** model has 'title, 'order', and 'completed' attributes
+
+app.Todo = Backbone.Model.extend({
+
+	defaults: {
+		title: '',
+		completed: false
+	},
+
+	toggle: function() {
+		this.save({
+			completed: !this.get('completed');
+		});
+	}
+});
